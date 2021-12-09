16ea5b90e21732c4d9bbfb47b97934145bce038b
Sveta <Sveta@KOMP>
2021-12-09 21:05:15 +0300

    Добавьте файлы проекта.

---
+++ 18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] bracket = str.ToCharArray();
+            int namber = 0;
+            foreach (char b in bracket)
+            {
+                if (b == '(' || b == '<' || b == '[' || b == '{')
+                {
+                    namber++;
+                }
+                else if (b == ')' || b == '>' || b == ']' || b == '}')
+                {
+                    namber--;
+                }
+            }
+            if (namber == 0)
+            {
+                Console.WriteLine("Скобки расставлены правильно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены не правильно");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 18__/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Stack> sk = new List<Stack>();
+            {
+                string str = Console.ReadLine();
+            }
+            foreach (var sko in sk)
+            {
+                if (sko == '[' || sko == '(' || sko == '{')
+            }
+
+
+        }
+    }
+    public class Stack
+    {
+        List<string> str { get; set; }
+    }
+}
